4cc561817311fa556f6804253daafb9982f27f15
Sebastian Sebald <sebastian.sebald@gmail.com>
2024-02-16 09:22:49 +0100

    eh?

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import { remarkCodeHike } from "@code-hike/mdx";
 export default defineConfig(async () => {
   const mdx = await import("@mdx-js/rollup");
   return {
+    // eslint-disable-next-line no-undef
+    base: process.env.NODE_ENV === "production" ? "/validation-slides" : "/",
     optimizeDeps: {
       include: ["react/jsx-runtime"],
     },
